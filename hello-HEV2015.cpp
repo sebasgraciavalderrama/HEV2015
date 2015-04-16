7a4866f976c45fba36cf86b1d2ba80b6dc97ccad
sebasgraciavalderrama <sebastiangracia123@gmail.com>
2015-04-16 17:27:44 -0500

    hello-HEV2015.cpp
    Archivo escrito en código C++

---
+++ hello-HEV2015.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello clase de herramientas!!" << endl;
+    cout << "Un saludo desde el repostitorio HEV2015 de Sebastian Gracia Valderrama!!!"<< endl;
+
+    return 0;
+}
